34c224521782c1408ae59ff532e20081d832966d
ksnyder2452 <31633014+ksnyder2452@users.noreply.github.com>
2025-11-09 09:42:46 -0500

    Add files via upload

---
+++ Health_Service/Health_Service.cs
@@ -0,0 +1,311 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Diagnostics;
+using System.Text;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.IO;
+
+string rabbitmq_server = ConfigurationManager.AppSettings["rabbitmq_home"];
+rabbitmq_server = rabbitmq_server.Trim();
+string rabbitmq_port = ConfigurationManager.AppSettings["rabbitmq_port"];
+rabbitmq_port = rabbitmq_port.Trim();
+string rabbitmq_user = ConfigurationManager.AppSettings["rabbitmq_user"];
+rabbitmq_user = rabbitmq_user.Trim();
+string rabbitmq_pwd = ConfigurationManager.AppSettings["rabbitmq_pwd"];
+rabbitmq_pwd = rabbitmq_pwd.Trim();
+string service_name = ConfigurationManager.AppSettings["service_name"];
+service_name = service_name.Trim();
+
+
+string mysql_Server = ConfigurationManager.AppSettings["mysql_Server"];
+mysql_Server = mysql_Server.Trim();
+string mysql_Port = ConfigurationManager.AppSettings["mysql_Port"];
+mysql_Port = mysql_Port.Trim();
+string mysql_Database = ConfigurationManager.AppSettings["mysql_Database"];
+mysql_Database = mysql_Database.Trim();
+string mysql_User = ConfigurationManager.AppSettings["mysql_User"];
+mysql_User = mysql_User.Trim();
+string mysql_Password = ConfigurationManager.AppSettings["mysql_Password"];
+mysql_Password = mysql_Password.Trim();
+string db_connect_string = "Server=" + mysql_Server + "; Database=" + mysql_Database + "; Uid=" + mysql_User + "; Pwd=" + mysql_Password + "; Port=" + mysql_Port;
+
+var uuidList = new List<string> { };
+
+Console.WriteLine(" Press [enter] to exit.");
+while (true)
+{
+    //Check if logger_service is available
+    if (await QueueExists("logger_service", rabbitmq_server))
+    {
+
+    }
+    else
+    {
+        string currentState = "";
+        using (MySqlConnection conn = new MySqlConnection(db_connect_string))
+        {
+            string select_framework_info = "select state from cast_state_tracker where name = 'logger_service'";
+            conn.Open();
+
+            using (MySqlCommand command = new MySqlCommand(select_framework_info, conn))
+            {
+                MySqlDataReader rdr = command.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    currentState = (string)rdr[0];
+                }
+                rdr.Close();
+            }
+        }
+        if (!currentState.Equals("OFFLINE") && !currentState.Equals("UNDER CONSTRUCTION"))
+        {
+            string update_cast_state_tracker = "update cast_state_tracker set state = 'OFFLINE', event_time_dt = NOW() where name = 'logger_service'";
+            updateRows(update_cast_state_tracker);
+        }
+    }
+
+    //Check if test_execution_service is available
+    if (await QueueExists("execution_service", rabbitmq_server))
+    {
+
+    }
+    else
+    {
+        string currentState = "";
+        using (MySqlConnection conn = new MySqlConnection(db_connect_string))
+        {
+            string select_framework_info = "select state from cast_state_tracker where name = 'execution_service'";
+            conn.Open();
+
+            using (MySqlCommand command = new MySqlCommand(select_framework_info, conn))
+            {
+                MySqlDataReader rdr = command.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    currentState = (string)rdr[0];
+                }
+                rdr.Close();
+            }
+        }
+        if (!currentState.Equals("OFFLINE") && !currentState.Equals("UNDER CONSTRUCTION"))
+        {
+            string update_cast_state_tracker = "update cast_state_tracker set state = 'OFFLINE', event_time_dt = NOW() where name = 'execution_service'";
+            updateRows(update_cast_state_tracker);
+        }
+    }
+
+    //Check if file_storage_service is available
+    if (await QueueExists("file_storage_service", rabbitmq_server))
+    {
+
+    }
+    else
+    {
+        string currentState = "";
+        using (MySqlConnection conn = new MySqlConnection(db_connect_string))
+        {
+            string select_framework_info = "select state from cast_state_tracker where name = 'file_storage_service'";
+            conn.Open();
+
+            using (MySqlCommand command = new MySqlCommand(select_framework_info, conn))
+            {
+                MySqlDataReader rdr = command.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    currentState = (string)rdr[0];
+                }
+                rdr.Close();
+            }
+        }
+        if (!currentState.Equals("OFFLINE") && !currentState.Equals("UNDER CONSTRUCTION"))
+        {
+            string update_cast_state_tracker = "update cast_state_tracker set state = 'OFFLINE', event_time_dt = NOW() where name = 'file_storage_service'";
+            updateRows(update_cast_state_tracker);
+        }
+    }
+
+    //Check if scheduler_service is available
+    if (await QueueExists("scheduler_service", rabbitmq_server))
+    {
+
+    }
+    else
+    {
+        string currentState = "";
+        using (MySqlConnection conn = new MySqlConnection(db_connect_string))
+        {
+            string select_framework_info = "select state from cast_state_tracker where name = 'scheduler_service'";
+            conn.Open();
+
+            using (MySqlCommand command = new MySqlCommand(select_framework_info, conn))
+            {
+                MySqlDataReader rdr = command.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    currentState = (string)rdr[0];
+                }
+                rdr.Close();
+            }
+        }
+        if (!currentState.Equals("OFFLINE") && !currentState.Equals("UNDER CONSTRUCTION"))
+        {
+            string update_cast_state_tracker = "update cast_state_tracker set state = 'OFFLINE', event_time_dt = NOW() where name = 'scheduler_service'";
+            updateRows(update_cast_state_tracker);
+        }
+    }
+
+
+    //Retrieve a list of all Test Client Services
+    uuidList.Clear();
+    using (MySqlConnection conn = new MySqlConnection(db_connect_string))
+    {
+        string select_framework_info = "select reference_uuid from logger where message like 'Started Client Service%' and display_name NOT LIKE 'SETUP New Client - IGNORE THIS ENTRY' order by order_in_system";
+        conn.Open();
+
+        using (MySqlCommand command = new MySqlCommand(select_framework_info, conn))
+        {
+            MySqlDataReader rdr = command.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                uuidList.Add((string)rdr[0]);
+            }
+            rdr.Close();
+        }
+    }
+    //Check if every Client Service is available
+    foreach (string currentUUID in uuidList)
+    {
+        Guid stateuuid = Guid.NewGuid();
+        string stateuuidAsString = stateuuid.ToString();
+        bool updateState = false;
+        using (MySqlConnection conn = new MySqlConnection(db_connect_string))
+        {
+            string state = "";
+            string select_framework_info = "select state, event_time_dt from current_state where reference_uuid = '" + currentUUID + "' order by order_in_system DESC limit 1";
+            conn.Open();
+
+            using (MySqlCommand command = new MySqlCommand(select_framework_info, conn))
+            {
+                MySqlDataReader rdr = command.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    state = (string)rdr[0];
+                    state = state.ToUpper();
+                    System.DateTime mySQLTime = (DateTime)rdr[1];
+                    System.DateTime dotnetTime = DateTime.Now;
+                    System.TimeSpan difference = dotnetTime.Subtract(mySQLTime);
+                    double totalMinutesDifference = difference.TotalMinutes;
+                    if ((state.StartsWith("COMPLETED ") && totalMinutesDifference > 30) || totalMinutesDifference > 720)
+                    {
+                        updateState = true;
+                    }
+                }
+                rdr.Close();
+            }
+        }
+        if (updateState)
+        {
+            string update_client_state_to_offline = "insert into current_state (uuid, reference_uuid, state, event_time_dt) values('" + stateuuidAsString + "', '" + currentUUID + "', 'OFFLINE', NOW())";
+            updateRows(update_client_state_to_offline);
+
+
+            string startDirectory = @"C:\Program Files\Rabbitmq Server\";
+            string targetFileName = "rabbitmqctl.bat";
+            string rabbitmqServerDirectory = "";
+
+            try
+            {
+                string[] files = Directory.GetFiles(startDirectory, targetFileName, SearchOption.AllDirectories);
+
+                if (files.Length > 0)
+                {
+                    string filePath = files[0];
+                    rabbitmqServerDirectory = Path.GetDirectoryName(filePath);
+                }
+                else
+                {
+                    Console.WriteLine($"File '{targetFileName}' not found in '{startDirectory}' or its subdirectories.");
+                }
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Console.WriteLine($"Access denied: {ex.Message}");
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine($"Directory not found: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+            try
+            {
+                Process rabbitMQCTL = new Process();
+                rabbitMQCTL.StartInfo.FileName = rabbitmqServerDirectory + Path.DirectorySeparatorChar + "rabbitmqctl.bat";
+                rabbitMQCTL.StartInfo.Arguments = " delete_queue client_service_" + currentUUID + " --if-empty";
+                Console.WriteLine("Run rabbitmqctl.bat delete_queue client_service_" + currentUUID + " --if-empty");
+                rabbitMQCTL.Start();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("rabbitmqctl failed to run, should not be considered a catastrophic failure. " + e.Message);
+            }
+        }
+    }
+    if (Console.KeyAvailable)
+    {
+        Console.ReadKey(true);
+        break;
+    }
+    Thread.Sleep(30000);
+}
+
+async Task<bool> QueueExists(string queueName, string hostName)
+{
+    try
+    {
+        var factory = new ConnectionFactory { HostName = hostName };
+        factory.UserName = rabbitmq_user;
+        factory.Password = rabbitmq_pwd;
+        using var connection = await factory.CreateConnectionAsync();
+        using (var channel = await connection.CreateChannelAsync())
+        {
+            await channel.QueueDeclarePassiveAsync(queueName);
+            return true;
+        }
+    }
+    catch (Exception e)
+    {
+        return false;
+    }
+    return false;
+}
+
+void updateRows(string updateStatement)
+{
+    using (MySqlConnection conn = new MySqlConnection(db_connect_string))
+    {
+        conn.Open();
+
+        int rowsAffected = 0;
+        using (MySqlCommand command = new MySqlCommand(updateStatement, conn))
+        {
+            try
+            {
+                rowsAffected = command.ExecuteNonQuery();
+            }
+            catch (MySqlException sqlE)
+            {
+                Console.WriteLine(sqlE.Message);
+            }
+        }
+    }
+}
