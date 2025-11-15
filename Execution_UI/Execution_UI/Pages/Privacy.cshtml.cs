f65197bb09a050522ff2003a98f07cd891770bc9
Kevin Snyder <kevin.howard.snyder@gmail.com>
2025-11-15 13:40:21 -0500

    Initial checkin of the Execution UI

---
+++ Execution_UI/Execution_UI/Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Test_Execution_UI.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
+
