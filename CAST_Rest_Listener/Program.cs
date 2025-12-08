cf20a22fd54a48369951ff266302e4459aa51965
Kevin Snyder <kevin.howard.snyder@gmail.com>
2025-12-08 17:32:44 -0500

    Added Simple REST Web Listener. Takes HTTP GET calls with ClientId as Parameter

---
+++ CAST_Rest_Listener/Program.cs
@@ -0,0 +1,107 @@
+using System.Text;
+using RabbitMQ.Client;
+
+var builder = WebApplication.CreateBuilder(args);
+
+/// <summary>
+/// The RabbitMQ Server pulled from appsettings.json
+/// </summary>
+string rabbitmq_home = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("AppSettings")["rabbitmq_home"];
+/// <summary>
+/// The RabbitMQ Port pulled from appsettings.json
+/// </summary>
+string rabbitmq_port = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("AppSettings")["rabbitmq_port"];
+/// <summary>
+/// The RabbitMQ UI Account pulled from appsettings.json
+/// </summary>
+string rabbitmq_user = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("AppSettings")["rabbitmq_user"];
+/// <summary>
+/// The RabbitMQ UI password pulled from appsettings.json
+/// </summary>
+string rabbitmq_pwd = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("AppSettings")["rabbitmq_pwd"];
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+app.MapRazorPages()
+   .WithStaticAssets();
+
+
+
+/// <summary>
+/// The RabbitMQ Connection Factory
+/// </summary>
+ConnectionFactory factory = new ConnectionFactory();
+factory.HostName = rabbitmq_home;
+factory.Port = int.Parse(rabbitmq_port);
+factory.UserName = rabbitmq_user;
+factory.Password = rabbitmq_pwd;
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+app.MapGet("/api/start_client", (string id) =>
+{
+    string message = "message for client_service_" + id + ": local: action: start run";
+    var body = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublishAsync(exchange: string.Empty, routingKey: "execution_service", body: body);
+    return Results.Ok("Client application started.");
+});
+
+app.MapGet("/api/stop_client", (string id) =>
+{
+    string message = "message for client_service_" + id + ": local: action: stop run";
+    var body = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublishAsync(exchange: string.Empty, routingKey: "execution_service", body: body);
+    return Results.Ok("Client application stopped.");
+});
+
+app.MapGet("/api/pause_client", (string id) =>
+{
+    string message = "message for client_service_" + id + ": local: action: pause run";
+    var body = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublishAsync(exchange: string.Empty, routingKey: "execution_service", body: body);
+    return Results.Ok("Client application paused.");
+});
+
+app.MapGet("/api/resume_client", (string id) =>
+{
+    string message = "message for client_service_" + id + ": local: action: resume run";
+    var body = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublishAsync(exchange: string.Empty, routingKey: "execution_service", body: body);
+    return Results.Ok("Client application resumed.");
+});
+
+app.MapGet("/api/abort_client", (string id) =>
+{
+    string message = "message for client_service_" + id + ": local: action: abort run";
+    var body = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublishAsync(exchange: string.Empty, routingKey: "execution_service", body: body);
+    return Results.Ok("Client application aborted.");
+});
+
+app.MapGet("/api/restart_client", (string id) =>
+{
+    string message = "message for client_service_" + id + ": local: action: restart run";
+    var body = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublishAsync(exchange: string.Empty, routingKey: "execution_service", body: body);
+    return Results.Ok("Client application restarted.");
+});
+
+app.Run();
